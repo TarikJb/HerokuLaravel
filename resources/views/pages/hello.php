63c3660057dcf966f84e95490f2431bcd83ec9e0
Tarik Jb <tarik.hani74@gmail.com>
2021-05-19 20:07:14 +0100

    init commit

---
+++ resources/views/pages/hello.php
@@ -0,0 +1,2 @@
+<h1>Project Normal</h1>
+<p>Learning laravel 8</p>
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// address et URL qui vous allez accéder
+/* Route::get('/hello',function(){
+    return "Hello World";
+}); */
+
+Route::get('/hello',function(){
+    /* return view("pages.hello"); */
+    return view("pages/hello");
+});
